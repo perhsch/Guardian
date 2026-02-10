de1ab525e6ee0c1b1624a6ced71b655124ef02a7
brennanmhr06 <b25851265@gmail.com>
2026-02-10 18:29:02 -0500

    [UPDATE] Code comment

--- index.js
+++ index.js
@@ -194,21 +194,30 @@ client.on('messageCreate', (message) => {
     if (message.mentions.has(client.user)) {
         // Build an informative embed
         const embed = new Discord.EmbedBuilder()
-            .setColor(0x5865F2)
+            .setColor(0x5865f2)
             .setTitle("Hello! I'm Guardian 🤖")
-            .setDescription('Thanks for mentioning me!\n\n**My default prefix is:** `/` (I use [slash commands](https://discord.com/developers/docs/interactions/application-commands))\n\nUse `/help` to get a list of commands or explore the resources below!')
+            .setDescription(
+                'Thanks for mentioning me!\n\n**My default prefix is:** `/` (I use [slash commands](https://discord.com/developers/docs/interactions/application-commands))\n\nUse `/help` to get a list of commands or explore the resources below!'
+            )
             .addFields(
                 { name: 'Quick Start', value: 'Type `/help` for a full command list.' },
-                { name: 'Support Server', value: '[Click here](https://discord.gg/)' },
+                { name: 'Support Server', value: '[Click here](https://discord.gg/)' } // your discord support server
             )
-            .setFooter({ text: `Guardian Bot • Made for your server!`, iconURL: client.user.displayAvatarURL() });
+            .setFooter({
+                text: `Guardian Bot • Made for your server!`,
+                iconURL: client.user.displayAvatarURL(),
+            });
 
         // Buttons for more actions/resources
         const row = new Discord.ActionRowBuilder().addComponents(
             new Discord.ButtonBuilder()
                 .setLabel('Invite Me')
                 .setStyle(Discord.ButtonStyle.Link)
-                .setURL('https://discord.com/oauth2/authorize?client_id=' + client.user.id + '&permissions=8&scope=bot%20applications.commands'),
+                .setURL(
+                    'https://discord.com/oauth2/authorize?client_id=' +
+                        client.user.id +
+                        '&permissions=8&scope=bot%20applications.commands'
+                ),
             new Discord.ButtonBuilder()
                 .setLabel('Support Server')
                 .setStyle(Discord.ButtonStyle.Link)
