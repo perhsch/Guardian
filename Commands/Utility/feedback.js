6cc835388c40d32f825ab76b718c8d375ef8021e
Brennan <b25851265@gmail.com>
2026-02-27 23:57:41 -0500

    [UPD] ID

--- Commands/Utility/feedback.js
+++ Commands/Utility/feedback.js
@@ -12,7 +12,7 @@ module.exports = {
             option.setName('rating').setDescription('Your rating out of 5.').setRequired(true)
         ),
     async execute(interaction, client, dbGuild) {
-        const feedbackChannel = client.channels.cache.get('1463689198824456347'); // Channel id of the feedback channel
+        const feedbackChannel = client.channels.cache.get('1471348003297300662'); // Channel id of the feedback channel
         const feedback = interaction.options.getString('feedback');
         const rating = interaction.options.getInteger('rating');
         feedbackChannel.send(
